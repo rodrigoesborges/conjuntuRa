bc9b8f3452350447a6a77a5c6bbe17a977d1a681
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2018-10-02 10:25:39 -0500

    baixada estatística de subutilização (equivalente ao desemprego total do dieese)
    desagregada para valores mensais
    
    Adicionada à série e ao gráfico

--- Nacional-Brasil/mercadodetrabalhonacional01.R
+++ Nacional-Brasil/mercadodetrabalhonacional01.R
@@ -4,6 +4,8 @@ library(ggplot2)
 library(scales)
 library(dynlm)
 library(tidyverse)
+library(zoo)
+library(tempdisagg)
 
 #idioma portugues
 Sys.setlocale(category = "LC_TIME", locale = "pt_BR.UTF-8")
@@ -12,17 +14,35 @@ Sys.setlocale(category = "LC_TIME", locale = "pt_BR.UTF-8")
 desemprego <- API_SIDRA(6381,variavel = "4099" )
 
 #transforma data para formato data com lubridate
-desemprego$data <- lubridate::as_date(paste(desemprego$`Trimestre Móvel (Código)`,"01"),"%Y%m%d")
+desemprego$data <- as.Date(paste(desemprego$`Trimestre Móvel (Código)`,"01"),"%Y%m%d")
 
 desemprego <- select(desemprego, data, Variável, Valor)
-colnames(desemprego) <- c("data","indicador","valor")
-desemprego$indicador <- gsub("^Taxa composta .*$","Taxa de subutilização",desemprego$indicador)
+colnames(desemprego) <- c("data","indicador","desocupacao")
 desemprego$indicador <- gsub("^Taxa de d.*$","Taxa de desocupação",desemprego$indicador)
+
+desempregots <- ts(desemprego$desocupacao, start = c(2012,03), frequency = 12)
+
+#baixa estimativas de desemprego total, ou taxa composta de subutilização, e desagrega mês a mês
+subutilizacao <- API_SIDRA(4099, variavel = "4118")
+subutilizacao$data <- as.Date(as.yearqtr(subutilizacao$Trimestre, format = "%qº trimestre %Y"))
+subutilizacao <- select(subutilizacao, data, Variável, Valor)
+subutilizacao$Variável <- gsub("^Taxa composta .*$","Taxa de subutilização",subutilizacao$Variável)
+colnames(subutilizacao) <- c("data","indicador","subutilizacao")
+
+
+
+submensal <- predict(td(subutilizacao$subutilizacao ~ 1, method = "denton-cholette", conversion = "average", to = 3))
+
+subutilizacaom <- ts(submensal,start = c(2012,01), freq = 12)
+
+desempregos <- ts.intersect(desempregots,subutilizacaom)
+desempregos <- data.frame(valores = (as.matrix(desempregos)), data = as.Date(time(desempregos)))
+
 
 #plota em um gráfico básico com bolas, 
 # adaptado de https://analisemacro.com.br/economia/dados-macroeconomicos/baixando-dados-do-sidra-com-o-r-o-pacote-sidrar/
 
-ggplot(desemprego, aes(x = data, y = valor, color = indicador))+
+ggplot(desempregos, aes(x = data, y = valor, color = indicador))+
   theme_classic()+
   geom_line(mapping = aes(x = data, y = valor, color = indicador))+
   scale_fill_discrete (name="indicador")+
