8adb4e4e17453acb6dd2275574bc7b5b6bf1c375
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2018-10-03 16:45:57 -0500

    primeiros testes com microdados, pacote microdadosBrasil

--- Nacional-Brasil/mercadodetrabalhonacional09microdados.R
+++ Nacional-Brasil/mercadodetrabalhonacional09microdados.R
@@ -1,2 +1,5 @@
 library('microdadosBrasil')
-download_sourceData("RAIS", 2016)
+#download_sourceData("RAIS", 2017) possivel com fork rodrigoesborges/microdadosBrasil
+#necessários praticamente 24gb de RAm para esta etapa, não possivel juntar todo o ano, 
+# portanto, salvo em condicoes de trabalho especiais
+#read_RAIS('vinculos', i = 2017)
