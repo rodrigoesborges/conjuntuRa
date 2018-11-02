18563dd13709869b8490c53a96915adf2d806814
Rodrigo Borges <rodrigo@imperialismoedependencia.org>
2018-11-02 15:38:15 -0500

    Acertos no código para gerar os valores

--- Nacional-Brasil/03_mercado_de_trabalho-RAIS-CAGED.R
+++ Nacional-Brasil/03_mercado_de_trabalho-RAIS-CAGED.R
@@ -4,12 +4,11 @@ library(readxl)
 library(dplyr)
 
 #Específico para projeto de Camilla
-CNAE95 <-read_xlsx("~/Nextcloud/Academia/Congressos e Revistas/2019-tese-camila/CNAE 95 _div.xlsx", col_names = TRUE)
+CNAE95 <-read_xlsx("~/RLocalData/CNAE 95 _div.xlsx", col_names = TRUE)
 #divisões específicas solicitadas por seu projeto - levaram a incluir um loop adicional
 # e coluna a mais nos resultados compilados
 divisoes <- sprintf("%02d",CNAE95$"Código")
-diretoriodoprojeto <- getwd()
-setwd("~/RLocalData")
+
 
 #ideal em seguida: procurar no csv anos que faltem,
 #do mais recente para trás, e só atualizar o csv com anos faltantes
@@ -22,21 +21,24 @@ proxysuper <- data.frame(matrix(ncol = 5, nrow = 0))
 colnames(proxysuper) <-nomescols
 
 for (ano in anos) {
-for (uf in ufs) {
-  rais <- read_RAIS("vinculos", i = ano, UF = uf)
-  for (divisao in divisoes){
-  raisocupas <- rais[grep(paste("$",divisao),rais$"CNAE 95 Classe")]
-  seden <- nrow(raisocupas)
-  if (nrow(raisocupas) == 0) { seden = 1 }
-  raissuper <- raisocupas[parse_number(raisocupas$"Vl Remun M\xe9dia (SM)", locale = locale("br", decimal_mark = ",")) < 2]
-  senum <- nrow(raissuper)
-  superexp <- senum/seden
-  novovalor <- data.frame(ano, uf, divisao,superexp,nrow(raisocupas))
-  names(novovalor) <- nomescols
-  proxysuper <- rbind(proxysuper,novovalor)
-  write.csv(proxysuper,paste0("seriesuperexp",anos[1],ano,".csv"))
-}
-}
+  for (uf in ufs) {
+    rais <- read_RAIS("vinculos", i = ano, UF = uf, root_path = "~/RLocalData/")
+    rais$"CNAE 95 Classe" <- sprintf("%05d",rais$"CNAE 95 Classe")
+    for (divisao in divisoes){
+      linhas <- substr(rais$"CNAE 95 Classe",1,2) == divisao
+      raisocupas <- rais[linhas,]
+      raisocupas$"Vl Remun M\xe9dia (SM)" <- parse_number(raisocupas$"Vl Remun M\xe9dia (SM)", locale = locale("br", decimal_mark = ","))
+      seden <- nrow(raisocupas)
+      if (nrow(raisocupas) == 0) { seden = 1 }
+      raissuper <- raisocupas[ raisocupas$"Vl Remun M\xe9dia (SM)" < 2]
+      senum <- nrow(raissuper)
+      superexp <- senum/seden
+      novovalor <- data.frame(ano, uf, divisao,superexp,nrow(raisocupas))
+      names(novovalor) <- nomescols
+      proxysuper <- rbind(proxysuper,novovalor)
+    }
+    write.csv(proxysuper,paste0("seriesuperexp",anos[1],ano,".csv"))
+  }
 }
 
 
