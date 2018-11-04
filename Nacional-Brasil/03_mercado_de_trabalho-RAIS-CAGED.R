0163f07681c489bf4fb07bc8bb21970c83d747a8
rodrigoesborges <rodrigoesborges@gmail.com>
2018-11-04 10:04:53 -0200

    codigo otimizado rais

--- Nacional-Brasil/03_mercado_de_trabalho-RAIS-CAGED.R
+++ Nacional-Brasil/03_mercado_de_trabalho-RAIS-CAGED.R
@@ -10,11 +10,9 @@ CNAE95 <-read_xlsx("~/RLocalData/CNAE 95 _div.xlsx", col_names = TRUE)
 divisoes <- sprintf("%02d",CNAE95$"Código")
 
 
-#ideal em seguida: procurar no csv anos que faltem,
-#do mais recente para trás, e só atualizar o csv com anos faltantes
-
 anos <- seq(1994,2017,1)
-ufs <- c("AC","AL","AM","AP","BA","CE", "DF", "ES", "GO", "MA","MG","MS","MT","PA","PB","PE","PR","RJ","RO","RR","RS","SC","SE","SP","TO")
+ufs <- c("AC","AL","AM","AP","BA","CE", "DF", "ES", "GO", "MA","MG","MS","MT","PA","PB","PE","PI","PR","RN","RJ","RO","RR","RS","SC","SE","SP","TO")
+#ufs <- "SP"
 nomescols <- c("ano","uf","divisao","percentual","vinculos")
 
 proxysuper <- data.frame(matrix(ncol = 5, nrow = 0))
@@ -22,28 +20,26 @@ colnames(proxysuper) <-nomescols
 
 for (ano in anos) {
   for (uf in ufs) {
-    rais <- try(read_RAIS("vinculos", i = ano, UF = uf, root_path = "~/RLocalData/"))
-    if (inherits(rais,"try-error")) next
+    rais <- read_RAIS("vinculos", i = ano, UF = uf, root_path = "~/RLocalData/", vars_subset = c("CNAE 95 Classe","Faixa Remun M\xe9dia (SM)"))
     rais$"CNAE 95 Classe" <- sprintf("%05d",parse_number(rais$"CNAE 95 Classe"))
     for (divisao in divisoes){
       linhas <- substr(rais$"CNAE 95 Classe",1,2) == divisao
       raisocupas <- rais[linhas,]
-      raisocupas$"Vl Remun M\xe9dia (SM)" <- parse_number(raisocupas$"Vl Remun M\xe9dia (SM)", locale = locale("br", decimal_mark = ","))
+      raisocupas$"Faixa Remun M\xe9dia (SM)" <- as.numeric(raisocupas$"Faixa Remun M\xe9dia (SM)")
       seden <- nrow(raisocupas)
       if (nrow(raisocupas) == 0) { seden = 1 }
-      raissuper <- raisocupas[ raisocupas$"Vl Remun M\xe9dia (SM)" < 2]
+      raissuper <- raisocupas[ raisocupas$"Faixa Remun M\xe9dia (SM)" <= 3]
       senum <- nrow(raissuper)
       superexp <- senum/seden
       novovalor <- data.frame(ano, uf, divisao,superexp,nrow(raisocupas))
       names(novovalor) <- nomescols
       proxysuper <- rbind(proxysuper,novovalor)
     }
-    write.csv(proxysuper,paste0("data/seriesuperexp",anos[1],ano,".csv"))
+    write.csv2(proxysuper,paste0("data/seriesuperexp",anos[1],"-",ano,".csv"))
+    gc()
   }
 }
 
 
-write.csv(proxysuper,paste0("data/seriesuperexp",anos[1],uf,ano,".csv"))
-
-
+write.csv2(proxysuper,paste0("data/seriesuperexp",anos[1],"-",ano,".csv"))
 
