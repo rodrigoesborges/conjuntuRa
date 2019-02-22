20cd29b42b3089910b2e676df77092aa1d5e096c
rodrigoesborges <rodrigoesborges@gmail.com>
2019-02-22 10:29:05 -0300

    PNAD - tentativas recentes de utilização do pacote microdadosBrasil para sua leitura.

--- Nacional-Brasil/03_mercado_de_trabalho_nacional-PNAD.R
+++ Nacional-Brasil/03_mercado_de_trabalho_nacional-PNAD.R
@@ -18,15 +18,33 @@ nomescols <- c("ano","uf","divisao","percentual","vinculos")
 proxysuper <- data.frame(matrix(ncol = 5, nrow = 0))
 colnames(proxysuper) <-nomescols
 
+pnad2001 <- read_rds("~/RLocalData/PNAD/2001 main.rds")
+
+# library(lodown)
+# # examine all available PNAD microdata files
+# pnad_cat <-
+#   get_catalog( "pnad" ,
+#                output_dir = file.path( path.expand( "~" ) , "PNAD" ) )
+# 
+# # 2011 only
+# pnad_cat <- subset( pnad_cat , year == 2011 )
+# # download the microdata to your local computer
+# pnad_cat <- lodown( "pnad" , pnad_cat )
+
+
+
+
+
 ####teste leitura
 
-anoteste <- 2002
-download_sourceData("PNAD",anoteste, unzip = T)
+# dirbase <- "~/RLocalData/PNAD"
+# anoteste <- 1976
+# download_sourceData("PNAD",anoteste, root_path = dirbase, unzip = T)
+# 
+# teste <- read_PNAD("pessoas", root_path = dirbase, anoteste)
+#                    
 
-teste <- read_PNAD("domicilios", anoteste)
-                   
-
-for (ano in anos) {
+#for (ano in anos) {
     #pnad <- read_PNAD("pessoas", i = ano, root_path = "~/RLocalData/PNAD"))
     # pnadocupas <- pnad
     # pnadocupas$"Faixa Remun M\xe9dia (SM)" <- as.numeric(pnadocupas$"Faixa Remun M\xe9dia (SM)")
@@ -40,7 +58,14 @@ for (ano in anos) {
     # proxysuper <- rbind(proxysuper,novovalor)
     # write.csv2(proxysuper,paste0("data/serie2sm",anos[1],"-",ano,".csv"))
     # gc()
-}
+#}
 
-write.csv2(proxysuper,paste0("data/serie2smPNAD",anos[1],"-",ano,".csv"))
+#write.csv2(proxysuper,paste0("data/serie2smPNAD",anos[1],"-",ano,".csv"))
 
+###########PNAD CONTINUA COM microdadosbrasil
+
+### se não houver sido baixado localmente
+microdadosBrasil::get_available_datasets(
+  
+)
+download_sourceData("PnadContinua","2017-1q",root_path = "~/RLocalData/PNADc/")
